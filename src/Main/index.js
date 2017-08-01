3121b114c0e331fef50cd353c9fb390b76e68737
Marc White <mwhite@overstock.com>
2017-08-01 10:23:54 -0600

    adding page transitions

--- src/Main/index.js
+++ src/Main/index.js
@@ -3,19 +3,38 @@ import ReactDOM from 'react-dom';
 import Home from '../Home';
 import About from '../About';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { RouteTransition } from 'react-router-transition';
 
 // const title = 'Marc White is Awesome';
 
 function App(props){
     return(
       <Router>
-          <Switch>
-            <Route exact="true" path="/" render={() => (
-                <h1>Welcome</h1>
-            )} />
-            <Route path="/home" component={Home}/>
-            <Route path="/about" component={About}/>
-          </Switch>
+          <div>
+          <ul>
+            <li><Link to="/">Main</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/home">Home</Link></li>
+          </ul>
+          <Route render={({location, history, match}) => {
+              return(
+                  <RouteTransition
+                    pathname={location.pathname}
+                    atEnter={{ opacity: 0 }}
+                    atLeave={{ opacity: 0 }}
+                    atActive={{ opacity: 1 }}
+                    >
+                    <Switch key={location.key} location={location} >
+                        <Route exact path="/" render={() => (
+                        <h1>Welcome</h1>
+                        )} />
+                        <Route exact path="/home" component={Home}/>
+                        <Route exact path="/about" component={About}/>
+                    </Switch>
+                </ RouteTransition>
+              );
+          }} />
+          </div>
       </Router>
     )
 }
